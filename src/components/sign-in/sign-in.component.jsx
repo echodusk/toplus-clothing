ddcb98bb4f87247f8eac7273c1c50a34f92fa2cf
Tomas Mejia <tomas.mejia.ed@gmail.com>
2019-12-27 20:26:29 -0300

    Ran prettier to prettify code

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -24,10 +24,10 @@ class SignIn extends React.Component {
 
     try {
       await auth.signInWithEmailAndPassword(email, password);
-      this.setState({ email: '', password: ''})
+      this.setState({ email: "", password: "" });
     } catch (err) {
       console.log(err);
-    };
+    }
   };
 
   handleChange = event => {
