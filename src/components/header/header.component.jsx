7ef53dd71b90aadfd843d8263d1265cc09d7dfca
Tomas Mejia <tomas.mejia.ed@gmail.com>
2019-12-28 09:52:08 -0300

    Created cart reducer and added hidden state to display or show cart dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { connect } from 'react-redux';
-import { auth } from '../../firebase/firebase.utils';
+import { connect } from "react-redux";
+import { auth } from "../../firebase/firebase.utils";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
-import CartIcon from '../cart-icon/cart-icon.component';
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 import "./header.styles.scss";
 
-const Header = ({ currentUser }) => (
-
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -21,19 +21,24 @@ const Header = ({ currentUser }) => (
       <Link className="option" to="/shop">
         CONTACT
       </Link>
-      {
-        currentUser ?
-        <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
-        :
-        <Link className="option" to="/signin">SIGN IN</Link>
-      }
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
+      )}
       <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
-})
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
+});
 
 export default connect(mapStateToProps)(Header);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
-import { createStore, applyMiddleware, compose} from 'redux'; 
+import { createStore, applyMiddleware } from 'redux'; 
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger, ];
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
