1285e495b3a7c2c9a2ce363e9f5ca61f1446f866
Tomas Mejia <tomas.mejia.ed@gmail.com>
2020-01-03 17:58:02 -0300

    Updating stripe integration, favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_RlJskBYAS7YsL9cNrukXHaGp00otNLr60O";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="TOPLUS Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
