9f7f44f70a224b54c0c736afdc449e07a031b43a
Tomas Mejia <tomas.mejia.ed@gmail.com>
2020-01-14 22:42:51 -0300

    Add fetchCollection generator for shop.sagas

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 import logger from "redux-logger";
+import createSagaMiddleware from 'redux-saga';
+import { fetchCollectionsStart } from './shop/shop.sagas'
 import thunk from 'redux-thunk';
 
 import rootReducer from "./root-reducer";
 
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
@@ -14,6 +17,8 @@ if (process.env.NODE_ENV === "development") {
 // const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(fetchCollectionsStart)
 
 export const persistor = persistStore(store);
 
