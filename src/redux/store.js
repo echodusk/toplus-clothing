51cb351fa0141e4bdc83c5c33a0ae300fb289286
Tomas Mejia <tomas.mejia.ed@gmail.com>
2020-01-19 10:50:39 -0300

    Transform userSignIn to use sagas

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,9 @@ import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 import createSagaMiddleware from 'redux-saga';
-import { fetchCollectionsStart } from './shop/shop.sagas'
-import thunk from 'redux-thunk';
+// import thunk from 'redux-thunk';
+
+import rootSaga from './root-saga';
 
 import rootReducer from "./root-reducer";
 
@@ -18,7 +19,7 @@ if (process.env.NODE_ENV === "development") {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-sagaMiddleware.run(fetchCollectionsStart)
+sagaMiddleware.run(rootSaga)
 
 export const persistor = persistStore(store);
 
