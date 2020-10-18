b2725aab28eb9413d603890538926aea47539edf
Tomas Mejia <tomas.mejia.ed@hotmail.com>
2020-10-18 02:00:23 -0300

    separate cra app into client folder

---
+++ client/src/components/header/header.styles.jsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const HeaderContainer = styled.div`
+  height: 70px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 25px;
+`;
+
+export const LogoContainer = styled(Link)`
+  height: 100%;
+  width: 330px;
+  padding: 25px;
+  display: flex;
+  align-items: center;
+
+  h1 {
+    margin-left: 15px;
+  }
+`;
+
+export const OptionsContainer = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+export const OptionLink = styled(Link)`
+  cursor: pointer;
+  padding: 10px 15px;
+  font-weight: bold;
+`;
