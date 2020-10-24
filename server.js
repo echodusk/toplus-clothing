23ba6007bf3a97b4229a7c69e488da4401a3f4e5
Tomas Mejia <tomas.mejia.ed@hotmail.com>
2020-10-23 22:06:39 -0300

    add tests (one)

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
@@ -20,6 +19,8 @@ app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(compression());
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function(req, res) {
@@ -29,9 +30,8 @@ if (process.env.NODE_ENV === 'production') {
 
 app.listen(port, error => {
   if (error) throw error;
-  console.log('Server running on port ' + port);
+  console.log('Server is running on port ' + port);
 });
-
 app.get('/service-worker.js', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
 });
